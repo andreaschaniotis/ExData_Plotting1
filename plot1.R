246c7d78ac48219904353fc22c73d0b29fe4a190
andreaschaniotis <andreas.chaniotis@gmail.com>
2021-09-22 01:04:24 +0200

    commit project

---
+++ plot1.R
@@ -0,0 +1,28 @@
+# plot 1
+rm(list=ls())
+# 
+#Define file name to save and target url
+zipfilename<-"Power_Dataset.zip"
+zipfileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+#check if already downloaded and un-ziped  before, otherwise download and unzip the data
+if (!file.exists(zipfilename)){
+  download.file(url=zipfileurl, destfile=zipfilename)
+  unzip(zipfilename) 
+}
+powerfile <- "household_power_consumption.txt"
+# read file with data
+all_data <- read.table(file=powerfile, header=TRUE, sep = ";", na.strings = "?", colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+# calculate time from date and time and add newtime column in the data
+all_data$newtime <-  strptime(paste(all_data$Date, all_data$Time, sep=" "),format= "%d/%m/%Y %H:%M:%S")
+# change format for date
+all_data$Date=as.Date(all_data$Date, format="%d/%m/%Y")
+# create final data
+final_data <- subset(all_data, Date >= "2007-02-01" & Date<="2007-02-02")
+
+#png device
+png(file='plot1.png',width = 480,height = 480)
+hist(final_data$Global_active_power, main = "Global Active Power", 
+     xlab = 'Global Active Power (kilowatts)', col = 'red')
+dev.off()
+
---
+++ plot4.R
@@ -0,0 +1,35 @@
+# plot 4
+
+rm(list=ls())
+# 
+#Define file name to save and target url
+zipfilename<-"Power_Dataset.zip"
+zipfileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+#check if already downloaded and un-ziped  before, otherwise download and unzip the data
+if (!file.exists(zipfilename)){
+  download.file(url=zipfileurl, destfile=zipfilename)
+  unzip(zipfilename) 
+}
+powerfile <- "household_power_consumption.txt"
+# read file with data
+all_data <- read.table(file=powerfile, header=TRUE, sep = ";", na.strings = "?", colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+# calculate time from date and time and add newtime column in the data
+all_data$newtime <-  strptime(paste(all_data$Date, all_data$Time, sep=" "),format= "%d/%m/%Y %H:%M:%S")
+# change format for date
+all_data$Date=as.Date(all_data$Date, format="%d/%m/%Y")
+# create final data
+final_data <- subset(all_data, Date >= "2007-02-01" & Date<="2007-02-02")
+
+#png device
+png(file='plot4.png',width = 480,height = 480)
+par(mfrow=c(2,2))
+plot(final_data$newtime,final_data$Global_active_power,xlab='',ylab='Global Active Power',type='l')
+plot(final_data$newtime,final_data$Voltage,type='l',xlab='datetime',ylab='Voltage')
+plot(final_data$newtime,final_data$Sub_metering_1,type='n',ylab="Energy Sub metering",xlab='')
+lines(final_data$newtime,final_data$Sub_metering_1)
+lines(final_data$newtime,final_data$Sub_metering_2,col = 'red')
+lines(final_data$newtime,final_data$Sub_metering_3,col = 'blue')
+legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"),bty='n')
+plot(final_data$newtime,final_data$Global_reactive_power,type='l',ylab='Global_reactive_power',xlab='datetime')
+dev.off()
